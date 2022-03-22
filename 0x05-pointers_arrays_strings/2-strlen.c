30d060f811bbe94e8fcd0ae98ba233384038be2d
Babayaga131 <troyvegas1@gmail.com>
2022-03-21 19:37:11 -0700

    2-strlen error fix

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -6,11 +6,11 @@
  * @s: the string to calculate it's length.
  * Return: length of a string.
  */
+
 int _strlen(char *s)
 {
 int i;
-int lenth = 0;
-
+int length = 0;
 for (i = 0; s[i] != '\0'; i++)
 {
 length++;
